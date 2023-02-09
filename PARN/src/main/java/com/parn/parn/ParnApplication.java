74f5dbe638e19515f22bd0f8a858a31595d4b4bf
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2023-02-09 14:36:13 +0100

    Creazione progetto e setup di dipendenze e connessioni

---
+++ PARN/src/main/java/com/parn/parn/ParnApplication.java
@@ -0,0 +1,13 @@
+package com.parn.parn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParnApplication.class, args);
+    }
+
+}
---
+++ PARN/src/test/java/com/parn/parn/ParnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parn.parn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
