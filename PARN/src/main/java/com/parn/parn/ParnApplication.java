d9bb41baa341876ae7e9875a54da597007fe11b1
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2023-02-09 15:45:15 +0100

    Test

--- PARN/src/main/java/com/parn/parn/ParnApplication.java
+++ PARN/src/main/java/com/parn/parn/ParnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParnApplication.class, args);
+        //Start the application
     }
 
 }
