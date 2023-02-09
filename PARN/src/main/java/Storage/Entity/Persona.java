65f8163c1600412c7cbfa94ff1bcaee216b84abb
DanyR2001 <danielerusso_@hotmail.it>
2023-02-09 16:12:21 +0100

    crezione entity persona e curriculm

---
+++ PARN/src/main/java/Storage/Entity/Persona.java
@@ -0,0 +1,6 @@
+package Storage.Entity;
+
+public class Persona extends Utente {
+
+
+}
---
+++ PARN/src/main/java/Storage/Entity/Utente.java
@@ -0,0 +1,23 @@
+package Storage.Entity;
+
+public abstract class Utente {
+    private String nome;
+    private String mail;
+    private String password;
+    private String regione;
+    private String provincia;
+    private int cap;
+    private String citta;
+    private String via;
+    private Long telefono;
+    private String foto;
+
+
+
+
+
+
+
+
+
+}
